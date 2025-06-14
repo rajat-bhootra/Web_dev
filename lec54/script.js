f5a910cb24cca70d67929b705360a77667617c14
Rajat Bhootra <166511592+rajat-bhootra@users.noreply.github.com>
2025-06-15 01:41:44 +0530

    JavaScript Introduction

---
+++ lec54/script.js
@@ -0,0 +1,17 @@
+alert("hello world!!!")
+
+console.log("code is running...")
+console.log("code is still running...")
+
+var a = prompt("Enter your number.")
+var isTrue = confirm("Do you want to leave this site")
+
+if(isTrue){
+    console.log("leaving the site...")
+}
+else{
+    console.log("not leaving...")
+}
+console.log("your number is " + a)
+
+document.body.style.backgroundColor = "cyan"
