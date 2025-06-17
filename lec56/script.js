4da5e32604346a9645cea3b44b05cfa8ac6fce88
Rajat Bhootra <166511592+rajat-bhootra@users.noreply.github.com>
2025-06-18 01:28:05 +0530

    JS Conditional

---
+++ lec56/script.js
@@ -0,0 +1,35 @@
+console.log("Conditional Tutorial");
+
+let age = 25;
+let grace = 4;
+
+age += grace
+console.log(age);
+
+
+console.log(age + grace)
+console.log(age - grace)
+console.log(age / grace)
+console.log(age * grace)
+console.log(age ** grace)
+console.log(age % grace) 
+
+
+if (age > 18){
+    console.log("You can drive.");
+}
+else{
+    console.log("You can't drive.");
+}
+
+console.log(3 === 3);
+console.log("4" === 3);
+
+console.log(true && false);
+
+
+let a = 5
+let b = 8
+
+console.log(a>b ? (a-b) : (b-a));
+
