22eecd948904a21c9abe2406df4a3fa5b0025f38
Rajat Bhootra <166511592+rajat-bhootra@users.noreply.github.com>
2025-06-29 17:49:26 +0530

    JS Strings

---
+++ lec60/string.js
@@ -0,0 +1,21 @@
+let name = "Rajat"
+
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+
+console.log(name.length);
+
+
+let str = "ki jai"
+console.log(`Bharat mata ${str}`);
+
+
+let a = "Rohan"
+console.log(a.toLocaleLowerCase());
+console.log(a.toLocaleUpperCase()); 
+console.log(a.slice(1,4));
+console.log(a.replace("R","M"));
+
+
+
